dab936540c55f44d58c8fade4b6171db5cc5171e
Amanda Dias <168840059+mnddevl@users.noreply.github.com>
2024-08-04 22:55:22 +0000

    finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,41 @@ import ReactDOM from "react-dom/client";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import SecondsCounter from "./component/home.jsx";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+let digits = [0, 0, 0, 0, 0, 0];
 
+// Función para actualizar el contador
+const updateCounter = () => {
+    let shouldContinue = true;
+    
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            shouldContinue = false;
+            break;
+        } else {
+            digits[i] = 9;
+        }
+    }
+
+    if (shouldContinue) {
+        // Empieza de 0
+        digits = [0, 0, 0, 0, 0, 0];
+    }
+
+    ReactDOM.createRoot(document.getElementById('app')).render(
+        <SecondsCounter 
+            digitoUno={digits[5]} 
+            digitoDos={digits[4]} 
+            digitoTres={digits[3]} 
+            digitoCuatro={digits[2]} 
+            digitoCinco={digits[1]} 
+            digitoSeis={digits[0]} 
+        />
+    );
+};
+
+setInterval(updateCounter, 1000);
